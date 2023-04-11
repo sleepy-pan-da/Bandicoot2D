62ca191c55a6a108f2053ca2c4fcba39feb6d782
sleepy-pan-da <61502188+sleepy-pan-da@users.noreply.github.com>
2023-04-11 10:37:49 +0800

    Added screen transition
    -Added new font

--- Levels/LevelManager.gd
+++ Levels/LevelManager.gd
@@ -4,28 +4,36 @@ extends Node2D
 export var fruits_left : int = 0
 onready var endpt : EndPoint = $EndPoint
 onready var level_indicator : Label = $UI/LevelIndicator
+onready var screen_transition : ScreenTransition = $CanvasLayer/ScreenTransition
 
 func _ready() -> void:
-    level_indicator.update_label(name)
-    EventManager.connect("collected_fruit", self, "on_collected_fruit")
-    init_endpt_if_all_fruits_are_collected()
+	level_indicator.update_label(name)
+	EventManager.connect("collected_fruit", self, "on_collected_fruit")
+	screen_transition.connect("transitioned_out", self, "on_transitioned_out")
+	init_endpt_if_all_fruits_are_collected()
+	screen_transition.transition_in()
 
 
 func init_endpt_if_all_fruits_are_collected() -> void:
-    if fruits_left == 0: endpt.enable()
+	if fruits_left == 0: endpt.enable()
 
 
 func on_collected_fruit() -> void:
-    fruits_left -= 1
-    enable_endpt_if_all_fruits_are_collected()
+	fruits_left -= 1
+	enable_endpt_if_all_fruits_are_collected()
 
 
 func enable_endpt_if_all_fruits_are_collected() -> void:
-    if fruits_left == 0:
-        AudioManager.playSfx("EnabledEndPoint")
-        endpt.enable()
+	if fruits_left == 0:
+		AudioManager.playSfx("EnabledEndPoint")
+		endpt.enable()
 
 
 func _unhandled_input(_event: InputEvent) -> void:
-    if Input.is_action_just_pressed("reload"):
-        get_tree().reload_current_scene()
+	if Input.is_action_just_pressed("reload"):
+		screen_transition.transition_out()
+		# get_tree().reload_current_scene()
+
+
+func on_transitioned_out() -> void:
+	get_tree().reload_current_scene()
---
+++ Shaders/ScreenTransition.gd
@@ -0,0 +1,27 @@
+class_name ScreenTransition
+extends ColorRect
+
+onready var tween : Tween = $Tween
+var black_screen : bool = true
+signal transitioned_out
+
+func _ready():
+	material.set_shader_param("progress", 1)
+
+
+func transition_in() -> void:
+	material.set_shader_param("progress", 1)
+	tween.interpolate_property(material, "shader_param/progress", 1, 0, 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	tween.start()
+	black_screen = false
+
+
+func transition_out() -> void:
+	material.set_shader_param("progress", 1)
+	tween.interpolate_property(material, "shader_param/progress", 0, 1, 0.5, Tween.TRANS_LINEAR, Tween.EASE_IN_OUT)
+	tween.start()
+	black_screen = true
+
+
+func _on_Tween_tween_all_completed() -> void:
+	if black_screen: emit_signal("transitioned_out")
