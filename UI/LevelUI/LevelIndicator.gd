255b3f630c9164012a7217f59b4f1552bfebfd87
sleepy-pan-da <61502188+sleepy-pan-da@users.noreply.github.com>
2023-04-12 12:36:21 +0800

    Centered level layout

--- UI/LevelUI/LevelIndicator.gd
+++ UI/LevelUI/LevelIndicator.gd
@@ -4,4 +4,4 @@ extends Label
 
 func update_label(level_name : String) -> void:
 	var level_number : String = level_name.split("Level")[1]
-	text = level_number + "/15"
+	text = level_number + "/18"
